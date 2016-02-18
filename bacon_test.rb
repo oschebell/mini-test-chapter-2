cf4c5369a3dea9fc7d2e21f04c85c9aaca70c76c
oschebell <oschebell@gmail.com>
2016-02-18 22:41:23 +1100

    Add mini-test

---
+++ bacon_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+class Bacon
+  def self.saved?
+    false
+  end
+end
+
+class BaconTest < Minitest::Test
+  def test_saved
+    assert Bacon.saved?, "Our bacon was not saved :("
+  end
+end
